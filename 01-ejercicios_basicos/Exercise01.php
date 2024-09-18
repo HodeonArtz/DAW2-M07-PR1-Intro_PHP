9c4abc605fa6943e28714314b5adb6f7084d9f68
HodeonArtz <martidicianolold@gmail.com>
2024-09-18 19:39:03 +0200

    Exercise01 parte 1

---
+++ 01-ejercicios_basicos/Exercise01.php
@@ -0,0 +1,41 @@
+<?php 
+  $num1 = rand(-100,100);
+  $num2 = rand(-100,100);
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ejercicio 1</title>
+</head>
+
+<body>
+  <p>
+    1er Número:
+    <?php 
+      echo $num1
+    ?>
+  </p>
+  <p>
+    2er Número:
+    <?php 
+      echo $num2
+    ?>
+  </p>
+  <p>
+    <?php echo $num1 ?> + <?php echo $num2 ?> =
+    <?php echo $num1 + $num2; ?>
+  </p>
+  <p>
+    <?php echo $num1 ?> - <?php echo $num2 ?> =
+    <?php echo $num1 - $num2; ?>
+  </p>
+  <p>
+    <?php echo $num1 ?> / <?php echo $num2 ?> =
+    <?php echo $num1 / $num2; ?>
+  </p>
+</body>
+
+</html>
