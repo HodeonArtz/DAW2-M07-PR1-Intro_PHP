744c5fc6ffaccd88912573058e4b335642568ead
HodeonArtz <martidicianolold@gmail.com>
2024-09-18 19:54:58 +0200

    Exercise01 parte 3

--- 01-ejercicios_basicos/Exercise01.php
+++ 01-ejercicios_basicos/Exercise01.php
@@ -12,6 +12,7 @@
 </head>
 
 <body>
+  <!-- ------------------------------------------------------------- -->
   <p>
     1er Número:
     <?php 
@@ -36,11 +37,23 @@
     <?php echo $num1 ?> / <?php echo $num2 ?> =
     <?php echo $num1 / $num2; ?>
   </p>
+  <!-- ------------------------------------------------------------- -->
   <p>
     <?php echo $num1 ?> es <?php 
       echo $num1 > $num2 ? "mayor" : ($num1 < $num2 ? "menor" : "igual");
     ?> que <?php echo $num2 ?>
   </p>
+  <!-- ------------------------------------------------------------- -->
+  <?php if($num1 > 1 && $num2 > 1){ ?>
+  <p>
+    Area de un triángulo con base de <?php echo $num1 ?>cm y con altura de <?php echo $num2 ?>cm:
+    <b>
+      <?php 
+        echo ($num1 * $num2) / 2
+      ?>cm
+    </b>
+  </p>
+  <?php } ?>
 </body>
 
 </html>
