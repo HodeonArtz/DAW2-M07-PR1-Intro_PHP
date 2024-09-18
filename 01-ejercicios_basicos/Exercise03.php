c3d17a2e3f47ecfdad61e40eeb3fe9859f4fa5da
HodeonArtz <martidicianolold@gmail.com>
2024-09-18 20:06:16 +0200

    Ejercicio 3 parte 1

---
+++ 01-ejercicios_basicos/Exercise03.php
@@ -0,0 +1,18 @@
+<?php 
+  $num1 = rand(10,100);
+  $num2 = rand(10,100);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ejercicio 3</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
