79ccfb01e6a88b5c27f5f03239277bd2fc8bbff5
HodeonArtz <martidicianolold@gmail.com>
2024-09-20 19:02:05 +0200

    Ejercicio 5

---
+++ 01-ejercicios_basicos/Exercise05.php
@@ -0,0 +1,38 @@
+<?php 
+  $n_numeros_pares = 0;
+  $n_numeros_impares = 0;
+  $valor_generado = 0;
+  $total= 0;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ejercicio 05</title>
+</head>
+
+<body>
+  <p>
+    Números:
+    <?php 
+
+      do {
+        $valor_generado = rand(0,20);
+        if($valor_generado % 2 == 0) $n_numeros_pares++;
+        if($valor_generado % 2 != 0) $n_numeros_impares++;
+        echo "$valor_generado ";
+      } while (($total += $valor_generado) < 100);
+    ?>
+  </p>
+  <p>
+    Cantidad de números pares: <?php echo $n_numeros_pares ?>
+  </p>
+  <p>
+    Cantidad de números impares: <?php echo $n_numeros_impares ?>
+
+  </p>
+</body>
+
+</html>
