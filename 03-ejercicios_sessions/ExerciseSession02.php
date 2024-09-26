2869362848c16ea507511993fc23bca5ca6b6fc2
HodeonArtz <martidicianolold@gmail.com>
2024-09-26 18:07:18 +0200

    closes #10 PHP programado y Ejercicio 2 de sessions terminado

--- 03-ejercicios_sessions/ExerciseSession02.php
+++ 03-ejercicios_sessions/ExerciseSession02.php
@@ -1,5 +1,18 @@
 <?php 
-  
+session_start();
+   if(isset($_POST["submit"])){
+    switch ($_POST["submit"]) {
+      case 'Modify':
+        $_SESSION["arr_nums"][(int)$_POST["position"]] = $_POST["new_value"];
+        break;
+      case 'Reset':
+        unset($_SESSION["arr_nums"]);
+        break;
+    }
+  } 
+  if(!isset($_SESSION["arr_nums"])){
+    $_SESSION["arr_nums"] = [10,20,30];
+  } 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,18 +30,32 @@
   <form action="./ExerciseSession02.php" method="post">
     <label for="position">Position to modify:</label>
     <select name="position" id="position">
-
+      <option value="0">0</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
     </select>
     <br>
+    <br>
     <label for="new_value">New value:</label>
-    <input type="number" name="new_value" id="new_value">
+    <input type="number" name="new_value" id="new_value" value="0">
+    <br>
     <br>
     <input type="submit" name="submit" value="Modify">
     <input type="submit" name="submit" value="Average">
     <input type="submit" name="submit" value="Reset">
   </form>
   <!-- current array -->
-  <p></p>
+  <p>
+    Current array: <?php echo implode(", ", $_SESSION["arr_nums"]); ?>
+  </p>
+  <!-- average -->
+  <p>
+    <?php 
+      if(isset($_POST["submit"]) && $_POST["submit"]=="Average"){
+        echo array_reduce($_SESSION["arr_nums"], fn($acc, $num) => $acc + $num)/3;
+      }
+    ?>
+  </p>
 </body>
 
 </html>
